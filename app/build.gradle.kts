65e3dd75fc33cb648075ff32fe17a1809d16c716
toyota-m2k <toyota.mitsuki@gmail.com>
2025-04-27 19:10:41 +0900

    update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "io.github.toyota32k.binder"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
