5d4bd7a20a46a521fb6ec4c822908af22b94f9c8
toyota-m2k <toyota.mitsuki@gmail.com>
2025-08-05 11:49:14 +0900

    fix: gradle warning

--- libBinder/build.gradle.kts
+++ libBinder/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.android.library)
@@ -25,9 +27,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     testOptions {
         unitTests.isIncludeAndroidResources = true
     }
@@ -38,6 +37,11 @@ android {
     }
 }
 
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
 
 dependencies {
     implementation(libs.coreKtx)
