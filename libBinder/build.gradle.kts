5299c35d6db279017f411dd80154c83d891b2b6b
toyota-m2k <toyota.mitsuki@gmail.com>
2026-01-16 19:07:43 +0900

    chore: AGP 9.0.0

--- libBinder/build.gradle.kts
+++ libBinder/build.gradle.kts
@@ -1,12 +1,12 @@
+import com.android.build.api.dsl.LibraryExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.android.library)
     id("maven-publish")
 }
 
-android {
+configure<LibraryExtension> {
     namespace = "io.github.toyota32k.binder"
     compileSdk = 36
 
@@ -34,12 +34,6 @@ android {
         singleVariant("release") {
             withSourcesJar()
         }
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
